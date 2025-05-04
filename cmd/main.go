c9158c8d8a3f45e36b3af3d6b992acdd8efb4f80
Андрей Романов <human1488pwnz@yandex.ru>
2025-05-04 18:10:44 +0300

    Refactor main function to initialize database connection and update dependency injection for driver handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
 	"github.com/ARXXIII/f1-api/internal/handler"
 	"github.com/ARXXIII/f1-api/internal/repository"
 	"github.com/ARXXIII/f1-api/internal/service"
+	"github.com/ARXXIII/f1-api/pkg/db"
 )
 
 func main() {
-	// Конфигурация
-	supabaseURL := "https://your-project-id.supabase.co/rest/v1"
-	supabaseAPIKey := "your-supabase-api-key"
+	ctx := context.Background()
 
-	// DI — dependency injection
-	driverRepo := repository.NewDriverRepository(supabaseURL, supabaseAPIKey)
+	db.InitDB(ctx)
+	defer db.Conn.Close(ctx)
+
+	// var version string
+	// if err := db.Conn.QueryRow(ctx, "SELECT version()").Scan(&version); err != nil {
+	// 	log.Fatalf("Query failed: %v", err)
+	// }
+	// log.Println("Postgres version:", version)
+
+	driverRepo := repository.NewDriverRepository()
 	driverService := service.NewDriverService(driverRepo)
-	driverHandler := handler.NewDriverHandler(driverService)
+	driverHandler := handler.NewDriverHandler(ctx, driverService)
 
-	// Маршруты
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
 	http.HandleFunc("/driver", driverHandler.GetDrivers)
+	http.HandleFunc("/driver/", driverHandler.GetDriverByID)
 
-	log.Println("Server is running on :8080...")
+	log.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
