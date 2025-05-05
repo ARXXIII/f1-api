886d89a89663b79ba9f7e257d4fa445e224d50e9
Андрей Романов <human1488pwnz@yandex.ru>
2025-05-05 17:47:05 +0300

    fix: Update GetDriverByID method to use uuid.UUID for ID type

--- internal/service/driver_service.go
+++ internal/service/driver_service.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/ARXXIII/f1-api/internal/model"
 	"github.com/ARXXIII/f1-api/internal/repository"
+	"github.com/google/uuid"
 )
 
 type DriverService interface {
 	GetDrivers(ctx context.Context) ([]model.Driver, error)
-	GetDriverByID(ctx context.Context, id int) (*model.Driver, error)
+	GetDriverByID(ctx context.Context, id uuid.UUID) (*model.Driver, error)
 }
 
 type driverService struct {
@@ -24,6 +25,6 @@ func (s *driverService) GetDrivers(ctx context.Context) ([]model.Driver, error) 
 	return s.repo.GetAll(ctx)
 }
 
-func (s *driverService) GetDriverByID(ctx context.Context, id int) (*model.Driver, error) {
+func (s *driverService) GetDriverByID(ctx context.Context, id uuid.UUID) (*model.Driver, error) {
 	return s.repo.GetByID(ctx, id)
 }
