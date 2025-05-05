fa493020e58359388249ac17333b65aebfc15c5f
Андрей Романов <human1488pwnz@yandex.ru>
2025-05-05 17:47:00 +0300

    fix: Update GetByID method to use uuid.UUID for ID and enhance driver data retrieval

--- internal/repository/driver_repository.go
+++ internal/repository/driver_repository.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/ARXXIII/f1-api/internal/model"
 	"github.com/ARXXIII/f1-api/pkg/db"
+	"github.com/google/uuid"
 )
 
 type DriverRepository interface {
 	GetAll(ctx context.Context) ([]model.Driver, error)
-	GetByID(ctx context.Context, id int) (*model.Driver, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*model.Driver, error)
 }
 
 type driverRepo struct{}
@@ -19,7 +20,7 @@ func NewDriverRepository() DriverRepository {
 }
 
 func (r *driverRepo) GetAll(ctx context.Context) ([]model.Driver, error) {
-	rows, err := db.Conn.Query(ctx, `SELECT id, first_name, last_name, team, number FROM drivers`)
+	rows, err := db.Conn.Query(ctx, `SELECT id, first_name, last_name, date_of_birth, place_of_birth, number, debut, team, status FROM drivers`)
 	if err != nil {
 		return nil, err
 	}
@@ -28,20 +29,42 @@ func (r *driverRepo) GetAll(ctx context.Context) ([]model.Driver, error) {
 	var drivers []model.Driver
 	for rows.Next() {
 		var d model.Driver
-		if err := rows.Scan(&d.ID, &d.FirstName, &d.LastName, &d.Team, &d.Number); err != nil {
+		if err := rows.Scan(
+			&d.ID,
+			&d.FirstName,
+			&d.LastName,
+			&d.DateOfBirth,
+			&d.PlaceOfBirth,
+			&d.Number,
+			&d.Debut,
+			&d.Team,
+			&d.Status,
+		); err != nil {
 			return nil, err
 		}
 		drivers = append(drivers, d)
 	}
+
 	return drivers, nil
 }
 
-func (r *driverRepo) GetByID(ctx context.Context, id int) (*model.Driver, error) {
-	row := db.Conn.QueryRow(ctx, `SELECT id, first_name, last_name, team, number FROM drivers WHERE id=$1`, id)
+func (r *driverRepo) GetByID(ctx context.Context, id uuid.UUID) (*model.Driver, error) {
+	row := db.Conn.QueryRow(ctx, `SELECT id, first_name, last_name, date_of_birth, place_of_birth, number, debut, team, status FROM drivers WHERE id = $1`, id)
 
 	var d model.Driver
-	if err := row.Scan(&d.ID, &d.FirstName, &d.LastName, &d.Team, &d.Number); err != nil {
+	if err := row.Scan(
+		&d.ID,
+		&d.FirstName,
+		&d.LastName,
+		&d.DateOfBirth,
+		&d.PlaceOfBirth,
+		&d.Number,
+		&d.Debut,
+		&d.Team,
+		&d.Status,
+	); err != nil {
 		return nil, err
 	}
+
 	return &d, nil
 }
