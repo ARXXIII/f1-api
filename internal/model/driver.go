c1049bc819c5d8b98a14960ea188bf44c7d2d17e
Андрей Романов <human1488pwnz@yandex.ru>
2025-05-05 17:46:48 +0300

    fix: Update Driver struct to use uuid.UUID and time.Time for improved type safety

--- internal/model/driver.go
+++ internal/model/driver.go
@@ -1,13 +1,19 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Driver struct {
-	ID           int    `json:"id"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	DateOfBirth  string `json:"date_of_birth"`
-	PlaceOfBirth string `json:"place_of_birth"`
-	Number       int    `json:"number"`
-	Debut        string `json:"debut"`
-	Team         string `json:"team"`
-	Status       string `json:"status"`
+	ID           uuid.UUID `json:"id"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	DateOfBirth  time.Time `json:"date_of_birth"`
+	PlaceOfBirth string    `json:"place_of_birth"`
+	Number       int       `json:"number"`
+	Debut        time.Time `json:"debut"`
+	Team         uuid.UUID `json:"team"`
+	Status       string    `json:"status"`
 }
