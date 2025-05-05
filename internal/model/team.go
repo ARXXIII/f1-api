59b994452b2cf8c6808a0a30c9887731edf23c9a
Андрей Романов <human1488pwnz@yandex.ru>
2025-05-05 17:46:53 +0300

    fix: Update Team struct to use uuid.UUID and time.Time for improved type safety

--- internal/model/team.go
+++ internal/model/team.go
@@ -1,10 +1,16 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Team struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Engine  string `json:"engine"`
-	Chassis string `json:"chassis"`
-	Debut   string `json:"debut"`
-	Founder string `json:"founder"`
+	ID      uuid.UUID `json:"id"`
+	Name    string    `json:"name"`
+	Engine  string    `json:"engine"`
+	Chassis string    `json:"chassis"`
+	Debut   time.Time `json:"debut"`
+	Founder string    `json:"founder"`
 }
