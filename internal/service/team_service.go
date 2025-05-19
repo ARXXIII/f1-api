3632e573a9d53ae85261693032379f05f34486ac
Андрей Романов <human1488pwnz@yandex.ru>
2025-05-19 16:41:34 +0300

    feat: Update TeamService to include pagination support for team retrieval methods

--- internal/service/team_service.go
+++ internal/service/team_service.go
@@ -9,7 +9,10 @@ import (
 )
 
 type TeamService interface {
-	GetTeams(ctx context.Context) ([]model.Team, error)
+	GetTeam(ctx context.Context, page int) ([]model.Team, error)
+	GetTeamByName(ctx context.Context, name string, page int) ([]model.Team, error)
+	GetTeamByEngine(ctx context.Context, chassis string, page int) ([]model.Team, error)
+	GetTeamByChassis(ctx context.Context, engine string, page int) ([]model.Team, error)
 	GetTeamByID(ctx context.Context, id uuid.UUID) (*model.Team, error)
 }
 
@@ -21,8 +24,20 @@ func NewTeamService(r repository.TeamRepository) TeamService {
 	return &teamService{repo: r}
 }
 
-func (s *teamService) GetTeams(ctx context.Context) ([]model.Team, error) {
-	return s.repo.GetAll(ctx)
+func (s *teamService) GetTeam(ctx context.Context, page int) ([]model.Team, error) {
+	return s.repo.GetAll(ctx, page)
+}
+
+func (s *teamService) GetTeamByName(ctx context.Context, name string, page int) ([]model.Team, error) {
+	return s.repo.GetByName(ctx, name, page)
+}
+
+func (s *teamService) GetTeamByEngine(ctx context.Context, engine string, page int) ([]model.Team, error) {
+	return s.repo.GetByEngine(ctx, engine, page)
+}
+
+func (s *teamService) GetTeamByChassis(ctx context.Context, chassis string, page int) ([]model.Team, error) {
+	return s.repo.GetByChassis(ctx, chassis, page)
 }
 
 func (s *teamService) GetTeamByID(ctx context.Context, id uuid.UUID) (*model.Team, error) {
